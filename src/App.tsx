b16a6a47f495dd906c64085b971dbec3bf617997
shandelier <26363920+Shandelier@users.noreply.github.com>
2021-12-19 00:30:48 +0100

    Idea of eval function

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import React from "react";
-type Args = { [argname: string]: boolean };
-type Operation = any; /* ...todo:
+// type Args = { [argname: string]: boolean };
+// type Operation = any;
+
+/* ...todo:
 a system for defining logical operations 
 (not, and, or... more if you want) that can be passed:
  - selected args by name: (X and Y)
@@ -11,6 +13,28 @@ a system for defining logical operations
 function evaluateOperation(operation: Operation, args: Args): boolean {
   /* ...todo: implement an evaluator for your operations, 
   given some args */
+  return false;
+}
+
+type Args = boolean | undefined | (() => boolean);
+type Operation = "OR" | "AND" | "NOT";
+
+function recEvaluateLogic(
+  x: Args,
+  y: Args,
+  operation: Operation
+): boolean | undefined {
+  // if arg is boolean -> return bool value
+  // if arg is undef -> break eval and throw undef on result
+  // if arg is function -> call this eval recursively and try evaluation on this arg
+
+  if (typeof x !== "boolean" || typeof y !== "boolean") {
+    if (x === undefined || y === undefined) {
+      return undefined;
+    } else if (x) {
+    }
+  }
+  return false;
 }
 
 function OperationBuilder(props: {
@@ -18,6 +42,7 @@ function OperationBuilder(props: {
   onChange: (value: Operation) => void;
 }): JSX.Element {
   /* ...todo: an ugly gui for creating operations */
+  return <div> Test {props} </div>;
 }
 
 export default function App() {
